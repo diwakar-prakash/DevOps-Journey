e1b46b502b6ad7ad36e1fe3c2943c42ea25f043e
diwakar-prakash <sush0697@gmail.com>
2025-11-22 15:14:48 +0530

    Learnings -- Docker -- 1st-compose-practice -- Connected the Redis container with the Backend Container via Docker Compose File

--- Learnings/Docker/1st-compose-practice/backend/app.js
+++ Learnings/Docker/1st-compose-practice/backend/app.js
@@ -1,10 +1,22 @@
 import express from "express";
+import Redis from "ioredis";
 
 const app = express();
 
-app.get('/', ( req, res ) => {
+const redis = new Redis({
+    // yahan par aapko ( matlab us ), we have to write the service name along with it's port
+    host : "redis",
+    port : 6379
+});
+
+app.get('/', async ( req, res ) => {
+    await redis.set("naam", "Diwakar, The Billionaire...");
+    const valueOfThingsWeSet = await redis.get("naam");
+
+
     res.status(200).json({
-        message : "Bool Bhai, kii haal hai"
+        message : "Bool Bhai, kii haal hai",
+        redisValue : valueOfThingsWeSet
     })
 })
 
