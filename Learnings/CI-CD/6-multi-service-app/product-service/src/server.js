20bb73b6cd273d975bbed2f4c6de7ba53e5fae37
diwakar-prakash <sush0697@gmail.com>
2026-03-29 20:11:30 +0530

    Learnings -- CI-CD -- 6-multi-service-app/product-service -- Did some changes in the product service to check weather the CD is working or not

--- Learnings/CI-CD/6-multi-service-app/product-service/src/server.js
+++ Learnings/CI-CD/6-multi-service-app/product-service/src/server.js
@@ -5,7 +5,8 @@ const app = express();
 app.get('/products', ( req , res ) => {
     res.status(200).json([
         {id: 1, product: "LAPTOP", price: 80000},
-        {id: 2, product: "Mobile", price: 35000}
+        {id: 2, product: "Mobile", price: 35000},
+        {id: 3, product: "AC", price: 40000}
     ]);
 });
 
