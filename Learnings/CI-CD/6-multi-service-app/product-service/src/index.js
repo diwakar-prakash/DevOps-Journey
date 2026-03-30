a562478234f29d0968641926add209ee64d4fb7f
diwakar-prakash <sush0697@gmail.com>
2026-03-30 23:59:17 +0530

    Learnings -- CI-CD -- 6-multi-service-app -- product-service -- Did some changes

--- Learnings/CI-CD/6-multi-service-app/product-service/src/index.js
+++ Learnings/CI-CD/6-multi-service-app/product-service/src/index.js
@@ -3,6 +3,6 @@ const app = require("./server");
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`The Product Service is running on the PORT ${PORT}`);
+    console.log(`The Product Service is running on the PORTS ${PORT}`);
 })
 
