8bf63362682ef22cc7ec318c95e6b1d3d678312e
diwakar-prakash <sush0697@gmail.com>
2026-04-06 20:13:07 +0530

    Learnings -- CI-CD -- 8-multi-micro-storage -- user-service -- Brother did some postgres modification.

--- Learnings/CI-CD/8-multi-micro-storage/user-service/src/index.js
+++ Learnings/CI-CD/8-multi-micro-storage/user-service/src/index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 
+const { Pool } = require("pg");
+
 const app = express();
+
+const pool = new Pool({
+  host: "postgres",
+  user: "admin",
+  password: "password",
+  database: "mydb",
+  port: 5432
+})
 
 app.get("/users", (req, res) => {
     res.status(200).json([
         { id: 1, name: "Donald" },
         { id: 2, name: "Kier" }
     ]);
+});
+
+app.get("/db-test", async ( req , res ) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch ( err ) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = app;
