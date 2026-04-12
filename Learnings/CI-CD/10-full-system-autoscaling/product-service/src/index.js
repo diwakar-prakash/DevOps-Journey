ee003ef23db4e9c74d15c485f7a8aa07ea6c6df1
diwakar-prakash <sush0697@gmail.com>
2026-04-12 16:37:18 +0530

    Learnings -- CI-CD -- 10-full-system-autoscaling -- product-service -- Wrote the application part of code for the product service

---
+++ Learnings/CI-CD/10-full-system-autoscaling/product-service/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+const APP_NAME = process.env.APP_NAME || "product service";
+const SECRET_KEY = process.env.SECRET_KEY || "default-secret";
+
+app.get('/products', ( req , res ) => {
+    res.status(200).json({
+        service : APP_NAME,
+        data : [
+            { id : 1, name : "LAPTOP", price : 1800 },
+            { id : 2, name : "Mobile", price : 1000 }
+        ]
+    })
+})
+
+app.get('/health', ( req , res ) => {
+    res.send("OK");
+})
+
+app.get('/ready', ( req , res ) => {
+    req.send("READY");
+})
+
+module.exports = app;
