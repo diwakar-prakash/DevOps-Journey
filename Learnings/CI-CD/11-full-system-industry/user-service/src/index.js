a2e0c2e1512d36cc9c78488342c4d3b88f4d6e67
diwakar-prakash <sush0697@gmail.com>
2026-04-15 17:03:52 +0530

    Learning -- CI-CD -- 11-full-system-industry -- user-service -- Wrote the user-service of the application part of the code.

---
+++ Learnings/CI-CD/11-full-system-industry/user-service/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const APP_NAME = process.env.APP_NAME || "user-service";
+
+app.get('/users', (req, res) => {
+    res.json({
+        service: APP_NAME,
+        data: [
+            { id: 1, name: "Vivek" },
+            { id: 2, name: "Harsh" },
+            { id: 3, name: "Himanshu" }
+        ]
+    });
+});
+
+app.get('/health', (req, res) => {
+    res.send("OK");
+});
+
+app.get('/ready', (req, res) => {
+    res.send("READY");
+});
+
+module.exports = app;
+
