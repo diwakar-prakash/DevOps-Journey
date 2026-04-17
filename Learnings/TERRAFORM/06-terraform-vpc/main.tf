c9e334d0725424760ecede16e45cbea517decf63
diwakar-prakash <sush0697@gmail.com>
2026-04-17 17:53:37 +0530

    Learnings -- TERRAFORM -- 06-terraform-vpc -- Created the VPC using Terraform

--- Learnings/TERRAFORM/06-terraform-vpc/main.tf
+++ Learnings/TERRAFORM/06-terraform-vpc/main.tf
@@ -51,3 +51,57 @@ resource "aws_route_table_association" "public_assoc" {
     route_table_id = aws_route_table.public_rt.id
 }
 
+resource "aws_eip" "nat_eip" {
+    domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.public_subnet.id
+
+    depends_on = [aws_internet_gateway.igw]
+
+    tags = {
+        Name = "my-nat"
+    }
+}
+
+resource "aws_route_table" "private_rt" {
+    vpc_id = aws_vpc.main_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat.id
+    }
+
+    tags = {
+        Name = "private-rt"
+    }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+    subnet_id = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_instance" "public_server" {
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.public_subnet.id
+    associate_public_ip_address = true
+
+    tags = {
+        Name = "public-server"
+    }
+}
+
+resource "aws_instance" "private_server" {
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.private_subnet.id
+
+    tags = {
+        Name = "private-server"
+    }
+}
+
---
+++ Learnings/TERRAFORM/06-terraform-vpc/variable.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
