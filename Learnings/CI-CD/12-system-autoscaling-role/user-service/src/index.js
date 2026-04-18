b20509d99b08a9953a5793d69879b171e9330f72
diwakar-prakash <sush0697@gmail.com>
2026-04-18 18:39:03 +0530

    Learnings -- CI-CD -- 12-system-autoscaling-role -- user-service -- Wrote the code for the user service part of the application

---
+++ Learnings/CI-CD/12-system-autoscaling-role/user-service/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const APP_NAME = process.env.APP_NAME;
+
+app.get('/users', ( req , res ) => {
+    res.status(200).json({
+        service : APP_NAME,
+        data : [
+            { id : 1, name : "Vivek" },
+            { id : 2, name : "Harsh" }
+        ]
+    });
+});
+
+app.get('/health', ( req , res ) => {
+    res.send("OK");
+})
+
+app.get('/ready', ( req , res ) => {
+    res.send("READY");
+})
+
+module.exports = app;
