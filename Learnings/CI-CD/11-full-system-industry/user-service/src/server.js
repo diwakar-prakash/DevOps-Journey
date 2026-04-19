5909d2b1da62bfde6986b8823493ab1e1955094a
diwakar-prakash <sush0697@gmail.com>
2026-04-19 20:16:23 +0530

    Learnings -- ci-cd -- 11-full-system-industry -- did some changes in the ci part of the application

--- Learnings/CI-CD/11-full-system-industry/user-service/src/server.js
+++ Learnings/CI-CD/11-full-system-industry/user-service/src/server.js
@@ -5,3 +5,5 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`User Service running on port ${PORT}`);
 });
+
+// This line is only to check weather the ci-cd is working properly or not..
