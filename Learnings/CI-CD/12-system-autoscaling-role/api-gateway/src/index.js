854928fafb63d1ed785bf7d5687b75516942e720
diwakar-prakash <sush0697@gmail.com>
2026-04-28 17:27:25 +0530

    Learnings -- CI-CD -- 12-system-autoscaling-role -- Added dockerignore, did some changes in the Dockerfile and also did some good level changes in the api-gateway

--- Learnings/CI-CD/12-system-autoscaling-role/api-gateway/src/index.js
+++ Learnings/CI-CD/12-system-autoscaling-role/api-gateway/src/index.js
@@ -6,14 +6,43 @@ const app = express();
 const USER_SERVICE = process.env.USER_SERVICE_URL;
 const PRODUCT_SERVICE = process.env.PRODUCT_SERVICE_URL;
 
+if(!USER_SERVICE || !PRODUCT_SERVICE) {
+    console.error("Missing USER_SERVICE_URL or PRODUCT_SERVICE_URL");
+    process.exit(1);
+}
+
+app.use((req, res, next) => {
+    console.log(JSON.stringify({
+        service: "api-gateway",
+        method: req.method,
+        url: req.url,
+        time: new Date().toISOString()
+    }));
+    next();
+});
+
 app.get('/users', async (req, res) => {
-    const response = await axios.get(`${USER_SERVICE}/users`);
-    res.json(response.data);
+    try {
+        const response = await axios.get(`${USER_SERVICE}/users`, {
+            timeout: 2000
+        });
+        res.json(response.data);
+    } catch ( err ) {
+        console.error("User Service Failed", err.message);
+        res.status(500).json({ message : "user service not working "});
+    }
 });
 
 app.get('/products', async (req, res) => {
-    const response = await axios.get(`${PRODUCT_SERVICE}/products`);
-    res.json(response.data);
+    try {
+        const response = await axios.get(`${PRODUCT_SERVICE}/products`, {
+            timeout: 2000
+        });
+        res.json(response.data);
+    } catch ( err ) {
+        console.error("Product Service Failed", err.message);
+        res.status(500).json({ message: "product service not working "});
+    }
 });
 
 app.get('/health', (req, res) => {
--- Learnings/CI-CD/12-system-autoscaling-role/api-gateway/src/server.js
+++ Learnings/CI-CD/12-system-autoscaling-role/api-gateway/src/server.js
@@ -1,7 +1,17 @@
 const app = require('./index');
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Api Gateway is running on the port ${PORT}`);
 })
+
+const shutdown = () => {
+    console.log("Shutting down api-gateway...");
+    server.close(() => {
+        process.exit(0);
+    });
+};
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
 
